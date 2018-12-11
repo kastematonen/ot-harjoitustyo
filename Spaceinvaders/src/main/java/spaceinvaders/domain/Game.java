c8cc51e2693d1568000a035cafd079b0f1c48b40
kastematonen <julia.nihtila@gmail.com>
2018-12-11 14:07:06 +0200

    testiluokat pakkauksiin

--- Spaceinvaders/src/main/java/spaceinvaders/domain/Game.java
+++ Spaceinvaders/src/main/java/spaceinvaders/domain/Game.java
@@ -204,7 +204,7 @@ public class Game {
         this.gameOn = false;
     }
     /**
-    * Jos pisteet tallennetaan tiedostoon metodin avulla. 
+    * Pisteet tallennetaan tiedostoon metodin avulla. 
     * 
     * @param   points   pisteet
     * 
