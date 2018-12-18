0e8732e3b9298595b3611d0c71ab5179adc8ac3b
kastematonen <40352564+kastematonen@users.noreply.github.com>
2018-12-18 16:52:18 +0200

    Create package-info.java

---
+++ Spaceinvaders/src/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Tietojen pysyväistallennus
+ */
+
+package spaceinvaders.dao;
+
+/**
+ * Sovelluslogiikka
+ */
+
+package spaceinvaders.domain;
+
+/**
+ * Käyttöliittymä
+ */
+
+package todoapp.ui;
